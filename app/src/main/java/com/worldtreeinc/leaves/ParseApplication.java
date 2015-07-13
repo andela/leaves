fbbdc9b00c7240805bee73c4a8552038cdab22dd
andela-bmustapha <mustapha.babtunde@andela.co>
2015-07-13 19:49:09 +0100

    Initial folder structure

--- app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
+++ app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
@@ -5,6 +5,10 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseCrashReporting;
+<<<<<<< HEAD
+=======
+import com.parse.ParseObject;
+>>>>>>> Initial folder structure
 import com.parse.ParseUser;
 
 public class ParseApplication extends Application {
@@ -15,10 +19,17 @@ public class ParseApplication extends Application {
         // Initialize Crash Reporting.
         ParseCrashReporting.enable(this);
 
+<<<<<<< HEAD
         // Enable Local Data store.
         Parse.enableLocalDatastore(this);
 
         // Add your initialization code here
+=======
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code heree
+>>>>>>> Initial folder structure
         Parse.initialize(this);
 
         ParseUser.enableAutomaticUser();
