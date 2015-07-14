eb7e4877bcea50a337c7c80c6227d6c8476cf982
andela-bmustapha <mustapha.babtunde@andela.co>
2015-07-14 10:19:11 +0100

    Fixed UI design bug #1

--- app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
+++ app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
@@ -5,13 +5,7 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseCrashReporting;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import com.parse.ParseObject;
->>>>>>> Initial folder structure
-=======
->>>>>>> Added library for material UI design
+
 import com.parse.ParseUser;
 
 public class ParseApplication extends Application {
@@ -22,24 +16,11 @@ public class ParseApplication extends Application {
         // Initialize Crash Reporting.
         ParseCrashReporting.enable(this);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         // Enable Local Data store.
         Parse.enableLocalDatastore(this);
 
         // Add your initialization code here
-=======
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
 
-        // Add your initialization code heree
->>>>>>> Initial folder structure
-=======
-        // Enable Local Data store.
-        Parse.enableLocalDatastore(this);
-
-        // Add your initialization code here
->>>>>>> Added library for material UI design
         Parse.initialize(this);
 
         ParseUser.enableAutomaticUser();
