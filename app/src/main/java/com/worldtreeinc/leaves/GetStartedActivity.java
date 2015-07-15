87081bfe570c4014037c502a5bb61ae87e560e78
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-15 15:29:56 +0100

    feature(logic): adds get started button logic [#98931768]

---
+++ app/src/main/java/com/worldtreeinc/leaves/GetStartedActivity.java
@@ -0,0 +1,26 @@
+package com.worldtreeinc.leaves;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class GetStartedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        final Intent welcomePage = new Intent(this, WelcomeActivity.class);
+        Button getStartedBtn = (Button) findViewById(R.id.get_started);
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(welcomePage);
+            }
+        });
+    }
+}
