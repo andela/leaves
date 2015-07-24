5177b97051c69f0a828624843c295ae13ba8d453
andela-bmustapha <mustapha.babtunde@andela.co>
2015-07-24 19:09:39 +0100

    feature(xml): creates Create Event page layout [#98934130]

--- app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
+++ app/src/main/java/com/worldtreeinc/leaves/ParseApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseCrashReporting;
-import com.parse.ParseUser;
 
 public class ParseApplication extends Application {
     @Override
