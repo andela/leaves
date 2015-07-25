2b0291d51d2a882818e3a5c46edb3ee162a4dbf1
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-25 10:37:44 +0100

    adds on click listener to the register_login link [#98929370]

--- app/src/main/java/com/worldtreeinc/leaves/RegisterActivity.java
+++ app/src/main/java/com/worldtreeinc/leaves/RegisterActivity.java
@@ -1,40 +1,28 @@
 package com.worldtreeinc.leaves;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends Activity {
+    private TextView register_login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        register_login_button = (TextView) findViewById(R.id.register_login_button);
+        register_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToLogin();
+            }
+        });
     }
 
     /**
@@ -43,18 +31,15 @@ public class RegisterActivity extends ActionBarActivity {
      *             and registers the user
      */
     public void register(View view) {
-
         // instantiate register object
         UserRegistration registerObject = new UserRegistration(this);
-
         // call register method
         registerObject.register();
-
     }
 
-    public void switchToLogin(View view) {
+    public void switchToLogin() {
         // switch to the login activity if the login button is clicked
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
