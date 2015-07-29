ec1ea5ec1eee007d49a22d985b1854411501fc2b
Kamiye <kamiye@Monsieur-Slim.local>
2015-07-29 10:34:18 +0100

    [#98935712] Build layout for Event details activity and add icons

--- app/src/main/java/com/worldtreeinc/leaves/EventDetailsActivity.java
+++ app/src/main/java/com/worldtreeinc/leaves/EventDetailsActivity.java
@@ -1,17 +1,66 @@
 package com.worldtreeinc.leaves;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
-public class EventDetailsActivity extends ActionBarActivity {
+public class EventDetailsActivity extends AppCompatActivity {
+
+    // declare class variables
+    private ParseItemsAdapter m_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        query.whereEqualTo("objectId", "U5pHVj7Z4E");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+                    //Log.d("score", "The getFirst request failed.");
+                } else {
+                    //Log.d("score", "Retrieved the object.");
+                    final ImageView banner = (ImageView) findViewById(R.id.event_details_banner);
+
+                    ParseFile eventBanner = (ParseFile) object.get("eventBanner");
+                    eventBanner.getDataInBackground(new GetDataCallback() {
+                        public void done(byte[] data, ParseException e) {
+                            if (e == null) {
+                                // data has the bytes for the resume
+                                // set the image file
+                                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                //Bitmap songImage = Bitmap.createScaledBitmap(bmp, 100, 100, true);
+                                banner.setImageBitmap(bmp);
+                            } else {
+                                // something went wrong
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        m_adapter = new ParseItemsAdapter(this);
+
+        ListView listView = (ListView) findViewById(R.id.items_list);
+        listView.setAdapter(m_adapter);
     }
 
     @Override
