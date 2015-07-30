ce8a2927dc7f33e8e2b68c4e0fe02ebe43530527
andela.kedeh <kingsley.edeh@andela.co>
2015-07-30 15:03:45 +0100

    fileCache testCase

---
+++ app/src/androidTest/java/com/worldtreeinc/leaves/FileCacheTest.java
@@ -0,0 +1,34 @@
+package com.worldtreeinc.leaves;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import java.io.File;
+
+/**
+ * Created by andela on 7/30/15.
+ */
+public class FileCacheTest extends AndroidTestCase {
+    private File cacheDir;
+    private Context context;
+    private String tString = "test";
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        FileCache fileCache = new FileCache(getContext());
+        try {
+            cacheDir = fileCache.getFile("test");
+
+
+        }catch(Exception e){
+            //Log.v("")
+            tString = "come";
+        }finally {
+            assertEquals(tString == "test", true);
+        }
+
+
+    }
+}
