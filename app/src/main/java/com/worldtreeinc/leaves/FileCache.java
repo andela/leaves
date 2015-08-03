2849c5b3b56ba568b2cb81404b9515d395eedac8
andela.kedeh <kingsley.edeh@andela.co>
2015-08-03 15:50:09 +0100

    updated FileCache.java

--- app/src/main/java/com/worldtreeinc/leaves/FileCache.java
+++ app/src/main/java/com/worldtreeinc/leaves/FileCache.java
@@ -1,7 +1,6 @@
 package com.worldtreeinc.leaves;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.io.File;
 
@@ -30,9 +29,8 @@ public class FileCache {
     public File getFile(String url) {
         String filename = String.valueOf(url.hashCode());
 
-        // String filename = URLEncoder.encode(url);
         File f = new File(cacheDir, filename);
-        Log.v("TAG", String.valueOf(url.hashCode()));
+
         return f;
 
     }
