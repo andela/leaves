5eac2fa6f64b57429972e7d02b462da86822d60c
andela.kedeh <kingsley.edeh@andela.co>
2015-08-04 13:47:29 +0100

    bug fix

--- app/src/main/java/com/worldtreeinc/leaves/ImageLoader.java
+++ app/src/main/java/com/worldtreeinc/leaves/ImageLoader.java
@@ -49,7 +49,6 @@ public class ImageLoader {
             queuePhoto(url, imageView);
             imageView.setImageResource(stub_id);
         }
-        imageView.setImageBitmap(bitmap);
     }
 
     private void queuePhoto(String url, ImageView imageView) {
