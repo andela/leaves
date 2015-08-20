2c1ea72bd1787f13b1c367b93b0b67205aa66649
andela-bmustapha <mustapha.babtunde@andela.co>
2015-08-20 15:57:29 +0100

    Completes item add form

---
+++ app/src/main/java/com/worldtreeinc/leaves/ItemListFragment.java
@@ -0,0 +1,21 @@
+package com.worldtreeinc.leaves;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andela on 8/20/15.
+ */
+public class ItemListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.item_list, container, false);
+    }
+}
