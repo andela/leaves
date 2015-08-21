6272b7b15510312cea2c5e571e9e9e95531a08ca
Kingsley Ochu <kingsley.ochu@andela.co>
2015-08-21 17:14:42 +0100

    Moves the Parse item query to the EventItem model [#98937634]

--- app/src/main/java/com/worldtreeinc/leaves/EventItem.java
+++ app/src/main/java/com/worldtreeinc/leaves/EventItem.java
@@ -1,20 +1,29 @@
 package com.worldtreeinc.leaves;
 
 import com.parse.ParseClassName;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 @ParseClassName("Bids")
 public class EventItem extends ParseObject {
     public EventItem() {
     }
 
-    public String getEventName() {
-        return getString("eventName");
+    public String getEventId() {
+        return getString("eventId");
     }
 
-    public void setEventName(String eventName) {
-        put("eventName", eventName);
+    public void setEventId(String eventId) {
+        put("eventId", eventId);
+    }
+
+    public void setUserId(String userId) {
+        put("userId", userId);
     }
 
     public Number getPreviousBid() {
@@ -47,5 +56,38 @@ public class EventItem extends ParseObject {
 
     public void setImage(ParseFile image) {
         put("itemImage", image);
+    }
+
+    public static EventItem getOne(String itemId) {
+        EventItem item;
+        ParseQuery<EventItem> query = ParseQuery.getQuery(EventItem.class);
+        try {
+            item = query.get(itemId);
+
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+            item = null;
+        }
+        return item;
+    }
+
+    public static List<EventItem> getAll(String... args) {
+        List<EventItem> item;
+        ParseQuery<EventItem> query = ParseQuery.getQuery(EventItem.class);
+        if (args.length == 0){
+            query.whereEqualTo("userId", ParseUser.getCurrentUser().getObjectId());
+        }
+        else if (args.length == 1){
+            query.whereEqualTo("eventId", args[0]);
+        }
+        try {
+            item = query.find();
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+            item = null;
+        }
+        return item;
     }
 }
