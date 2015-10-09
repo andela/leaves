c377cd03fc21ece93174a3400d2eb2a4c5628ede
andela-bmustapha <mustapha.babtunde@andela.co>
2015-10-09 12:07:22 +0100

    Adds test for BidHandler

--- app/src/androidTest/java/com/worldtreeinc/leaves/ItemBidHandlerTest.java
+++ app/src/androidTest/java/com/worldtreeinc/leaves/ItemBidHandlerTest.java
@@ -1,8 +1,9 @@
 package com.worldtreeinc.leaves;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityTestCase;
+
+
 
 /**
  * Created by andela on 10/2/15.
@@ -12,9 +13,26 @@ public class ItemBidHandlerTest extends ActivityInstrumentationTestCase2<Planner
     public ItemBidHandlerTest() {
         super(PlannerEventActivity.class);
     }
-
     public void testActivity(){
         PlannerEventActivity plannerEventActivity = getActivity();
         assertNotNull(plannerEventActivity);
+    }
+
+    public void testEventClick() {
+//        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(EventDetailsActivity.class.getName(), null, false);
+//        final PlannerEventActivity plannerEventActivity = getActivity();
+//        plannerEventActivity.runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+
+        // final PlannerEventActivity plannerEventActivity = getActivity();
+        Intent intent = new Intent();
+        intent.putExtra("OBJECT_ID", "VG9xAs6fuG");
+        intent.putExtra("IS_PLANNER", true);
+        intent.setClassName(getInstrumentation().getTargetContext(), EventDetailsActivity.class.getName());
+        getInstrumentation().startActivitySync(intent);
     }
 }
