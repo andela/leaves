d1d114f52a44fb8819b7ba41d63e913d83e5343f
andela-shassan <hassan.ayomon@andela.com>
2016-01-06 15:58:43 +0100

    Refactor Login and RoleOption activities

--- app/src/main/java/com/worldtreeinc/leaves/activity/LoginActivity.java
+++ app/src/main/java/com/worldtreeinc/leaves/activity/LoginActivity.java
@@ -13,6 +13,7 @@ import com.facebook.appevents.AppEventsLogger;
 import com.parse.ParseFacebookUtils;
 import com.worldtreeinc.leaves.R;
 import com.worldtreeinc.leaves.helper.UserAuthentication;
+import com.worldtreeinc.leaves.utility.ActivityLauncher;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText mUsername;
@@ -43,8 +44,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     userAuthentication.FacebookLogin();
                     break;
                 case R.id.registerUser:
+                    ActivityLauncher.runIntent(this, RegisterActivity.class);
+                    break;
                 case R.id.resetPassword:
-                    runIntent(v.getId());
+                    ActivityLauncher.runIntent(this, ResetPasswordActivity.class);
                     break;
             }
         } catch(Exception e){
@@ -52,15 +55,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private void runIntent(int activityId) {
-        Intent intent ;
-        if (activityId == R.id.resetPassword){
-            intent = new Intent(this, ResetPasswordActivity.class);
-        } else {
-            intent = new Intent(this, RegisterActivity.class);
-        }
-        startActivity(intent);
-    }
+
 
     private void initialise() {
         mUsername = (EditText) findViewById(R.id.usernameLoginTextBox);
@@ -77,7 +72,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         Button facebookLoginButton = (Button) findViewById(R.id.FacebookLoginButton);
         facebookLoginButton.setOnClickListener(this);
-
     }
 
     @Override
---
+++ app/src/main/java/com/worldtreeinc/leaves/utility/ActivityLauncher.java
@@ -0,0 +1,7 @@
+package com.worldtreeinc.leaves.utility;
+
+/**
+ * Created by Semiu on 06/01/2016.
+ */
+public class ActivityLauncher {
+}
