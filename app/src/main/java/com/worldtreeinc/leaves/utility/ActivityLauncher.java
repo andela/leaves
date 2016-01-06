eaa8182fa30672a14160a46aa8cf22a6abcd3874
andela-shassan <hassan.ayomon@andela.com>
2016-01-06 16:01:07 +0100

    Add ActivityLauncher to handle Intents

--- app/src/main/java/com/worldtreeinc/leaves/utility/ActivityLauncher.java
+++ app/src/main/java/com/worldtreeinc/leaves/utility/ActivityLauncher.java
@@ -1,7 +1,15 @@
 package com.worldtreeinc.leaves.utility;
 
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by Semiu on 06/01/2016.
  */
 public class ActivityLauncher {
+
+    public static void runIntent(Context context, Class<?> activityClass) {
+        Intent intent = new Intent(context, activityClass);
+        context.startActivity(intent);
+    }
 }
