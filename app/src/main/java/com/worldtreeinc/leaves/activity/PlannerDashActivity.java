54a328e1fc966af3fa1b366594af022d74b51645
andela <chidiebere.nwokocha@andela.com>
2016-03-09 06:46:06 +0100

    [#114659973] Add navigation drawer to planner dashboard

--- app/src/main/java/com/worldtreeinc/leaves/activity/PlannerDashActivity.java
+++ app/src/main/java/com/worldtreeinc/leaves/activity/PlannerDashActivity.java
@@ -3,6 +3,10 @@ package com.worldtreeinc.leaves.activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,7 +26,8 @@ import com.worldtreeinc.leaves.utility.ActivityLauncher;
 import java.util.ArrayList;
 
 
-public class PlannerDashActivity extends AppCompatActivity implements View.OnClickListener {
+public class PlannerDashActivity extends AppCompatActivity implements View.OnClickListener,
+    NavigationView.OnNavigationItemSelectedListener{
 
     private ListView bidList;
     private PlannerDashItemListAdapter listAdapter;
@@ -34,6 +39,7 @@ public class PlannerDashActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         checkLoggedInUser();
         setContentView(R.layout.activity_planner_dash);
+        setUpNavigationDrawer();
         initialize();
         new ItemAsyncTask().execute();
     }
@@ -53,6 +59,20 @@ public class PlannerDashActivity extends AppCompatActivity implements View.OnCli
         createEventBtn.setOnClickListener(this);
         Button manageEventBtn = (Button) findViewById(R.id.manage_events_btn);
         manageEventBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_bidder) {
+            ActivityLauncher.runIntent(this, BidderDashActivity.class);
+        } else if (id == R.id.nav_logout) {
+            User.logout();
+            ActivityLauncher.runIntent(this, WelcomeActivity.class);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private class ItemAsyncTask extends AsyncTask<Void, Void, Void> {
@@ -83,19 +103,14 @@ public class PlannerDashActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_planner_dash, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }else if(id == R.id.action_logout) {
@@ -121,5 +136,16 @@ public class PlannerDashActivity extends AppCompatActivity implements View.OnCli
         }
         Intent intent = new Intent(PlannerDashActivity.this, activitySwitch);
         startActivity(intent);
+    }
+
+    private void setUpNavigationDrawer() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
